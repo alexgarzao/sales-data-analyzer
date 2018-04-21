a2ea0aef62b5281449441364f344a8c05d3d8323
Alex S. Garzao <alex.garzao@zapcorp.com.br>
2018-04-21 13:16:51 -0300

    Initial commit

---
+++ src/main/java/sales_data_analyzer/App.java
@@ -0,0 +1,14 @@
+package sales_data_analyzer;
+
+/**
+* @author: Alex S. Garzão
+*
+* Main class. The data analyzer start here.
+*/
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Starting Sales Data Analyzer 0.1");
+    }
+}
