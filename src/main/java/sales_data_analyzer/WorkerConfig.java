86276dbd9787f278b883202b1bf5c016ccd7dedb
Alex S. Garzao <alex.garzao@zapcorp.com.br>
2018-04-22 18:23:25 -0300

    WorkerConfig, class to keeep config related to the work.

---
+++ src/main/java/sales_data_analyzer/WorkerConfig.java
@@ -0,0 +1,16 @@
+package sales_data_analyzer;
+
+public class WorkerConfig {
+
+    public WorkerConfig(String inFilename, String outFilename, String bkpFilename)
+    {
+        this.inFilename = inFilename;
+        this.outFilename = outFilename;
+        this.bkpFilename = bkpFilename;
+    }
+
+    // TODO: must be private, with get/set?
+    public String inFilename;
+    public String outFilename;
+    public String bkpFilename;
+}
