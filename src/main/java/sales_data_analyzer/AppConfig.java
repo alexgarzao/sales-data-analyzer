2ea1033fa6ce26a7a43b7c7957e9b188b76daef3
Alex S. Garzao <alex.garzao@zapcorp.com.br>
2018-04-23 03:43:19 -0300

    First release of AppConfig.

---
+++ src/main/java/sales_data_analyzer/AppConfig.java
@@ -0,0 +1,9 @@
+package sales_data_analyzer;
+
+public class AppConfig {
+    public static final String inPath = "./data/in";
+    public static final String outPath = "./data/out";
+    public static final String bkpPath = "./data/bkp";
+    public static final int maxWorkers = 20;
+    public static final String logFilename = "./data/log/sda.log";
+}
